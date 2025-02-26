1f0786cdfc8eecefcad55662f952d3480558e10a
Wolfgang Huß <wolle.huss@pjannto.com>
2025-02-26 08:30:38 +0100

    Update metadata

--- branding/constants/metadata.js
+++ branding/constants/metadata.js
@@ -4,7 +4,7 @@ export default {
   APPLICATION_SHORT_NAME: 'changemedia.at',
   APPLICATION_DESCRIPTION: 'changemedia.at Network driven by ocelot.social',
   COOKIE_NAME: 'changemedia-at-token',
-  ORGANIZATION_NAME: 'changemedia.at',
+  ORGANIZATION_NAME: 'changemedia',
   ORGANIZATION_JURISDICTION: 'Österreich',
   THEME_COLOR: 'rgb(23, 181, 63)', // $color-primary – as the main color in general. e.g. the color in the background of the app that is visible behind the transparent iPhone status bar to name one use case, or the current color of SVGs to name another use case
 }
